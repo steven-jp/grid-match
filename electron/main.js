5b967fa66ebc1546024ac9b9cd5435e0dcd512ae
steven-jp <stevenjp4@hotmail.com>
2021-04-26 18:47:44 -0700

    updated electron

--- electron/main.js
+++ electron/main.js
@@ -1,41 +1,36 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
 const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {},
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: { nodeIntegration: true },
   });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:3000");
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`,
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+app.on("ready", createWindow);
 
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
