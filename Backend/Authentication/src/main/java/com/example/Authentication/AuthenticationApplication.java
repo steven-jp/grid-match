e2c3f2cbcbf5e2b93031c114da4a6333ae735b92
steven-jp <stevenjp4@hotmail.com>
2021-09-15 19:10:09 -0700

    fixed issues with backend folder structure not recognizing packages

---
+++ Backend/Authentication/src/main/java/com/example/Authentication/AuthenticationApplication.java
@@ -0,0 +1,16 @@
+package com.example.Authentication;
+
+import com.example.Authentication.User.UserController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.Authentication.User","com.example.Authentication.Security"})
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
